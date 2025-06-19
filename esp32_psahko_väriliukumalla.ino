4cf0bdc7c018bccf37eb265841d9314c05651df5
aotiot <157993594+aotiot@users.noreply.github.com>
2025-06-19 16:14:30 +0300

    Update esp32_psahko_väriliukumalla.ino

--- esp32_psahko_väriliukumalla.ino
+++ esp32_psahko_väriliukumalla.ino
@@ -7,8 +7,8 @@
 #include <vector>
 
 // 🛜 WiFi-tiedot
-const char* ssid = "iottijotti";
-const char* password = "iotiotiot";
+const char* ssid = "ssid";
+const char* password = "password";
 
 // 💡 LED-matriisin asetukset
 #define LED_PIN     2
